5badfb823b6ed322b97812af253deb635cdb764a
Jorge Bucaran <mail@jorgebucaran.com>
2018-07-17 17:59:45 +0900

    2.1.1

--- getopts.d.ts
+++ getopts.d.ts
@@ -1,15 +1,8 @@
-/** Interface for getopts parsed options object. */
 interface ParsedOptions {
   _: string[]
   [key: string]: any
 }
 
-/** Interface for getops options argument.
- * @param alias Option aliases.
- * @param boolean Options that should be parsed as booleans.
- * @param default Default values for missing options.
- * @param unknown Run for every unknown option.
- */
 interface Options {
   alias?: { [key: string]: string | string[] }
   boolean?: string[]
@@ -17,12 +10,11 @@ interface Options {
   unknown?: (optionName: string) => boolean
 }
 
-/** Parse the arguments passed to your program from the command line.
+/**
  * @param argv Arguments to parse.
  * @param options Options.
- * @returns An object with parsed pptions.
+ * @returns An object with parsed options.
  */
-
 declare function getopts(argv: string[], options?: Options): ParsedOptions
 
 export = getopts
