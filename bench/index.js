4f454253f96327f8fe2737f676bad64a61d134a6
Jorge Bucaran <mail@jorgebucaran.com>
2018-08-24 17:26:01 +0900

    refactor: improve benchmark runner; remove load time tests
    - Add runBenchmark function to help running the benchmark.
    
    - Testing module loading times seems to be unreliable. I different results
      by changing the require order and the last module required is always the
      fastest to load regardless of which module it was.

--- bench/index.js
+++ bench/index.js
@@ -1,32 +1,21 @@
 const { Suite } = require("benchmark")
 
-console.log(`# Load Time`)
+const runBenchmark = (test, modules) =>
+  Object.keys(modules)
+    .reduce(
+      (suite, name) => suite.add(name, () => test(modules[name], name)),
+      new Suite().on("cycle", ({ target: { name, hz } }) =>
+        console.log(`${name} × ${Math.floor(hz).toLocaleString()} ops/sec`)
+      )
+    )
+    .run()
 
-console.time("mri")
-const mri = require("mri")
-console.timeEnd("mri")
-
-console.time("yargs")
-const yargs = require("yargs-parser")
-console.timeEnd("yargs")
-
-console.time("getopts")
-const getopts = require("..")
-console.timeEnd("getopts")
-
-console.time("minimist")
-const minimist = require("minimist")
-console.timeEnd("minimist")
-
-const argv = ["--super=8", "--no-slack", "-au9000", "--", "game", "over"]
-
-console.log(`\n# Parse Time`)
-new Suite()
-  .on("cycle", ({ target: { name, hz } }) =>
-    console.log(`${name} × ${Math.floor(hz).toLocaleString()} ops/sec`)
-  )
-  .add("mri", () => mri(argv))
-  .add("yargs", () => yargs(argv))
-  .add("getopts", () => getopts(argv))
-  .add("minimist", () => minimist(argv))
-  .run()
+runBenchmark(
+  parse => parse(["--turbo", "--no-kill", "-xw1000", "--", "alpha", "beta"]),
+  {
+    mri: require("mri"),
+    yargs: require("yargs-parser"),
+    getopts: require(".."),
+    minimist: require("minimist")
+  }
+)
