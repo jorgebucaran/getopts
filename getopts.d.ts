cc90712758f9cfc8aa1ae11c847dac7b9ef152f8
Ovyerus <iamovyerus@gmail.com>
2018-10-19 17:32:30 +1100

    refactor: export types for typescript
    Helps TypeScript users not have to redefine the "ParsedOptions" and "Options" interfaces in their own code in order to prevent TypeScript erroring when trying to return/use inferred types from getopts.

--- getopts.d.ts
+++ getopts.d.ts
@@ -1,20 +1,22 @@
-interface ParsedOptions {
-  _: string[]
-  [key: string]: any
-}
-
-interface Options {
-  alias?: { [key: string]: string | string[] }
-  boolean?: string[]
-  default?: { [key: string]: any }
-  unknown?: (optionName: string) => boolean
-}
-
 /**
  * @param argv Arguments to parse.
  * @param options Options.
  * @returns An object with parsed options.
  */
-declare function getopts(argv: string[], options?: Options): ParsedOptions
+declare function getopts(argv: string[], options?: getopts.Options): getopts.ParsedOptions
 
 export = getopts
+
+declare namespace getopts {
+  export interface ParsedOptions {
+    _: string[]
+    [key: string]: any
+  }
+  
+  export interface Options {
+    alias?: { [key: string]: string | string[] }
+    boolean?: string[]
+    default?: { [key: string]: any }
+    unknown?: (optionName: string) => boolean
+  }
+}
