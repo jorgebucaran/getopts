29b876b6a64f2da61fd76c4dcbc6bd5b20ea50d0
Jorge Bucaran <mail@jorgebucaran.com>
2021-02-14 01:35:36 +0900

    Modernize benchmarks

--- bench/index.js
+++ bench/index.js
@@ -1,21 +1,26 @@
-const { Suite } = require("benchmark")
+import bench from "benchmark"
+import getopts from "../index.js"
+import minimist from "minimist"
+import yargs from "yargs-parser"
+import mri from "mri"
 
 const runBenchmark = (test, modules) =>
   Object.keys(modules)
     .reduce(
       (suite, name) => suite.add(name, () => test(modules[name], name)),
-      new Suite().on("cycle", ({ target: { name, hz } }) =>
+      new bench.Suite().on("cycle", ({ target: { name, hz } }) =>
         console.log(`${name} × ${Math.floor(hz).toLocaleString()} ops/sec`)
       )
     )
     .run()
 
 runBenchmark(
-  parse => parse(["--turbo", "--no-slack", "-xyz1000", "--", "alpha", "beta"]),
+  (parse) =>
+    parse(["--turbo", "--no-slack", "-xyz1000", "--", "alpha", "beta"]),
   {
-    getopts: require(".."),
-    minimist: require("minimist"),
-    yargs: require("yargs-parser"),
-    mri: require("mri")
+    getopts,
+    minimist,
+    yargs,
+    mri,
   }
 )
