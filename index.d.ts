939f96fefd5986e11a96a312f0793a1f9846d10b
Nathan Wise <nathan88wise@gmail.com>
2025-07-03 14:10:07 +0100

    fix: change TS definition to work with ESM as per https://github.com/jorgebucaran/getopts/issues/58

--- index.d.ts
+++ index.d.ts
@@ -3,25 +3,21 @@
  * @param options Parsing options (configuration).
  * @returns The parsed arguments.
  */
-declare function getopts(
+export function getopts(
   argv: string[],
-  options?: getopts.Options
-): getopts.ParsedOptions
+  options?: Options
+): ParsedOptions
 
-export = getopts
+export interface ParsedOptions {
+  _: string[]
+  [key: string]: any
+}
 
-declare namespace getopts {
-  export interface ParsedOptions {
-    _: string[]
-    [key: string]: any
-  }
-
-  export interface Options {
-    alias?: { [key: string]: string | string[] }
-    string?: string[]
-    boolean?: string[]
-    default?: { [key: string]: any }
-    unknown?: (optionName: string) => boolean
-    stopEarly?: boolean
-  }
+export interface Options {
+  alias?: { [key: string]: string | string[] }
+  string?: string[]
+  boolean?: string[]
+  default?: { [key: string]: any }
+  unknown?: (optionName: string) => boolean
+  stopEarly?: boolean
 }
